158a11c40a1da9f0f9d7f58ce74b71c3ea369215
amorehead <amorehead@users.noreply.github.com>
2026-05-21 18:03:49 +0000

    Deploying to gh-pages from @ amorehead/amorehead.github.io@1d27b21e19db39a42e816e33bb1fcb459de39288 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-bridging-worlds",
+            },{id: "post-mixing-paradigms",
+      
+        title: 'Mixing paradigms <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+      
+      description: "(Highly subjective) AI4Science highlights of April 2026",
+      section: "Posts",
+      handler: () => {
+        
+          window.open("https://amorehead.substack.com/p/mixing-paradigms", "_blank");
+        
+      },
+    },{id: "post-bridging-worlds",
       
         title: "Bridging worlds",
       
