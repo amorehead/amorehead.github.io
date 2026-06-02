820418361d8bc2c87685c1ad3486cddf8ecd4342
amorehead <amorehead@users.noreply.github.com>
2026-06-02 13:36:21 +0000

    Deploying to gh-pages from @ amorehead/amorehead.github.io@d322c96cda7debed9c83688431232277d3eb4347 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,7 +51,18 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-mixing-paradigms",
+            },{id: "post-a-life-spent-living",
+      
+        title: "A life spent living",
+      
+      description: "An annotated reading of the books I finished in May 2026.",
+      section: "Posts",
+      handler: () => {
+        
+          window.location.href = "/blog/2026/a-life-spent-living/";
+        
+      },
+    },{id: "post-mixing-paradigms",
       
         title: 'Mixing paradigms <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
       
